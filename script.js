9458a00671fd471e25ef99bb56859b887afceb72
Sadaf-Daneshgar <sadaf.barekzai54321@gmail.com>
2024-10-24 09:39:22 +0430

    fix the linters error

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
-const imageElems = document.querySelectorAll(".image-container");
-const imageNameElems = document.querySelectorAll(".image-name");
-const preBtn = document.querySelector("#pre");
-const nextBtn = document.querySelector("#next");
+const imageElems = document.querySelectorAll('.image-container');
+const imageNameElems = document.querySelectorAll('.image-name');
+const preBtn = document.querySelector('#pre');
+const nextBtn = document.querySelector('#next');
 
 imageElems.forEach((imageElem, index) => {
-  imageElem.addEventListener("mouseover", () => {
-    imageNameElems[index].style.display = "flex";
+  imageElem.addEventListener('mouseover', () => {
+    imageNameElems[index].style.display = 'flex';
   });
 
-  imageElem.addEventListener("mouseout", () => {
-    imageNameElems[index].style.display = "none";
+  imageElem.addEventListener('mouseout', () => {
+    imageNameElems[index].style.display = 'none';
   });
 });
 
 let currentPage = 1;
-let imagesPerPage = 8; // Set to 6 images per page
+const imagesPerPage = 8;
 
-preBtn.addEventListener("click", () => {
-  if (currentPage > 1) {
-    currentPage--;
-    displayImages();
-  }
-});
-
-nextBtn.addEventListener("click", () => {
-  if (currentPage < Math.ceil(imageElems.length / imagesPerPage)) {
-    currentPage++;
-    displayImages();
-  }
-});
-
-// Function to display images based on the current page
 function displayImages() {
   const startIndex = (currentPage - 1) * imagesPerPage;
   const endIndex = startIndex + imagesPerPage;
 
   imageElems.forEach((imageElem, index) => {
     if (index >= startIndex && index < endIndex) {
-      imageElem.style.display = "block";
+      imageElem.style.display = 'block';
     } else {
-      imageElem.style.display = "none";
+      imageElem.style.display = 'none';
     }
   });
 }
 
-// Initial call to display images on the first page
 displayImages();
+
+preBtn.addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage -= 1;
+    displayImages();
+  }
+});
+
+nextBtn.addEventListener('click', () => {
+  if (currentPage < Math.ceil(imageElems.length / imagesPerPage)) {
+    currentPage += 1;
+    displayImages();
+  }
+});
